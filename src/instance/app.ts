a7d67634598df2f83197e8c0be4c49d77dba8bbf
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-07-14 20:15:29 -0300

    Added DotEnv configuration

--- src/instance/app.ts
+++ src/instance/app.ts
@@ -2,9 +2,11 @@
 import express from "express";
 const app = express();
 import cors from "cors";
+import dotenv from "dotenv";
 
 // Settings
 app.use(cors());
+dotenv.config();
 
 // Routes
 import routes from "../routes/routes"
