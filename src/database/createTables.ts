9945dc33797a253a9943d9470e873f97fa2c7f62
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-07-15 20:34:10 -0300

    Tables migration created

---
+++ src/database/createTables.ts
@@ -0,0 +1,62 @@
+// Modules
+import connection from "./connection";
+
+// Tables creation
+const createTables = async (): Promise<void> => {
+    try {
+        await connection.transaction(async transaction => {
+            transaction.raw(
+                "CREATE TABLE IF NOT EXISTS categories(" +
+                    "id SERIAL NOT NULL, " +
+                    "name VARCHAR(50) NOT NULL, " + 
+                    "createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(), " +
+                    "PRIMARY KEY (id)" +
+                ");"
+            );
+        
+            transaction.raw(
+                "CREATE TABLE IF NOT EXISTS articles(" +
+                    "id SERIAL NOT NULL, " +
+                    "title VARCHAR(100) NOT NULL, " +
+                    "description VARCHAR(200) NOT NULL, " +
+                    "article TEXT NOT NULL, " +
+                    "categoryId INTEGER NOT NULL, " +
+                    "authorId INTEGER NOT NULL, " +
+                    "slug VARCHAR(100) NOT NULL, " +
+                    "createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(), " +
+                    "PRIMARY KEY (id)" +
+                ");"
+            );
+        
+            transaction.raw(
+                "CREATE TABLE IF NOT EXISTS users(" +
+                    "id SERIAL NOT NULL, " +
+                    "name VARCHAR(20) NOT NULL, " +
+                    "email VARCHAR(50) NOT NULL, " +
+                    "password VARCHAR(20) NOT NULL, " +
+                    "isAdmin BOOLEAN NOT NULL DEFAULT false, " +
+                    "isEmailPublic BOOLEAN NOT NULL DEFAULT false, " +
+                    "profilePicture VARCHAR(255) DEFAULT NULL, " +
+                    "createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(), " +
+                    "PRIMARY KEY (id)" +
+                ");"
+            );
+
+            transaction.raw(
+                "CREATE TABLE IF NOT EXISTS commentaries(" +
+                    "id SERIAL NOT NULL, " +
+                    "commentary TEXT NOT NULL, " +
+                    "articleId INTEGER NOT NULL, " +
+                    "commentaryId INTEGER DEFAULT NULL, " +
+                    "authorId INTEGER NOT NULL, " +
+                    "createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(), " +
+                    "PRIMARY KEY (id)" +
+                ");"
+            );
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default createTables;
