af9910f2a783e959a1699b12551b646999e17d22
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-07-16 20:12:28 -0300

    Auto migration removed; migrations now need to be manually done

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 // Instance
 import app from "./instance/app";
 
-// Migrations
-import runMigrations from "./database/migrations";
-
 // Environment variables
 import dotenv from "dotenv";
 dotenv.config();
-const { APP_PORT, AUTO_RUN_MIGRATIONS } = process.env;
+const { APP_PORT } = process.env;
 
 // Listening
 app.listen(APP_PORT, () => {
     console.log(`Server online in localhost:${APP_PORT} at ${new Date()}`);
-    if (AUTO_RUN_MIGRATIONS === "true")
-        runMigrations();
 });
