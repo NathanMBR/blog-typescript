e4047545f04ea52120a6a631f0dcf2c6f6dacf9a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-07-16 13:13:42 -0300

    Migrations order changed for better viewing

--- src/database/migrations.ts
+++ src/database/migrations.ts
@@ -6,6 +6,20 @@ const createTables = async () => {
     try {
         await connection.transaction(async transaction => {
             await transaction.raw(
+                "CREATE TABLE IF NOT EXISTS users(" +
+                    "id SERIAL NOT NULL, " +
+                    "name VARCHAR(20) NOT NULL, " +
+                    "email VARCHAR(50) NOT NULL, " +
+                    "password VARCHAR(20) NOT NULL, " +
+                    "isAdmin BOOLEAN NOT NULL DEFAULT false, " +
+                    "isEmailPublic BOOLEAN NOT NULL DEFAULT false, " +
+                    "profilePicture VARCHAR(255) DEFAULT NULL, " +
+                    "createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(), " +
+                    "PRIMARY KEY (id)" +
+                ");"
+            );
+
+            await transaction.raw(
                 "CREATE TABLE IF NOT EXISTS categories(" +
                     "id SERIAL NOT NULL, " +
                     "name VARCHAR(50) NOT NULL, " + 
@@ -23,20 +37,6 @@ const createTables = async () => {
                     "categoryId INTEGER NOT NULL, " +
                     "authorId INTEGER NOT NULL, " +
                     "slug VARCHAR(100) NOT NULL, " +
-                    "createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(), " +
-                    "PRIMARY KEY (id)" +
-                ");"
-            );
-        
-            await transaction.raw(
-                "CREATE TABLE IF NOT EXISTS users(" +
-                    "id SERIAL NOT NULL, " +
-                    "name VARCHAR(20) NOT NULL, " +
-                    "email VARCHAR(50) NOT NULL, " +
-                    "password VARCHAR(20) NOT NULL, " +
-                    "isAdmin BOOLEAN NOT NULL DEFAULT false, " +
-                    "isEmailPublic BOOLEAN NOT NULL DEFAULT false, " +
-                    "profilePicture VARCHAR(255) DEFAULT NULL, " +
                     "createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(), " +
                     "PRIMARY KEY (id)" +
                 ");"
