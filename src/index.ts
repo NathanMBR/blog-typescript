0b0630b258937affb519ce0a24cec97a0a8e98a2
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-07-17 21:42:03 -0300

    Selected database message added

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import app from "./instance/app";
 // Environment variables
 import dotenv from "dotenv";
 dotenv.config();
-const { APP_PORT } = process.env;
+const { APP_PORT, IS_APP_IN_PRODUCTION } = process.env;
 
 // Listening
 app.listen(APP_PORT, () => {
+    console.log(`Connected with ${IS_APP_IN_PRODUCTION == "true" ? "production" : "development"} database`);
     console.log(`Server online in localhost:${APP_PORT} at ${new Date()}`);
 });
