f3c61183574ad84b3377665b726dd338168795dc
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-07-26 17:07:04 -0300

    index.ts bug correction (now correctly informs the database mode)

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import app from "./instance/app";
 // Environment variables
 import dotenv from "dotenv";
 dotenv.config();
-const { APP_PORT, IS_APP_IN_PRODUCTION } = process.env;
+const { APP_PORT, APP_MODE } = process.env;
 
 // Listening
 app.listen(APP_PORT, () => {
-    console.log(`Connected with ${IS_APP_IN_PRODUCTION == "true" ? "production" : "development"} database`);
+    console.log(`Connected with ${APP_MODE} database`);
     console.log(`Server online in localhost:${APP_PORT} at ${new Date()}`);
 });
